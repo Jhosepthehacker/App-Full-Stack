935cc0bc2f31b37bb3b4cacf3bcff914b13e3dc6
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-17 19:42:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+const sendRequests = () => {
+  alert("Hola");
+
+  async function getData() {
+    const API_URL = "http://127.0.0.1:8080/message";
+
+    const response = await fetch(API_URL, {
+      method: 'GET'
+    });
+
+    if (!response.ok) {
+      throw new Error(`Ha ocurrido un error: ${response.status}`);
+  }
+
+    const json = await response.json();
+    const text = json.message;
+
+    alert(`Respuesta del backend: ${text}`);
+    console.log(`Response Of Backend: ${text}
+  ${json}`
+    );
+  }
+
+  getData();
+}
