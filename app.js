c4f237efcfd4e2f1d98bf38d9df59684303c7c4f
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-11-17 19:41:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const dom = () => {
-  const head = document.head;
-  const body = document.body;
 
-  console.log(head, "\n", body);
-}
-
-dom();
-
-async function sendMessageToBackend() {
-  const API_URL = "http://192.168.1.102/8080/";
-  await fetch();
-}
